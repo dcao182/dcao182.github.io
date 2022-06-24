021c7421d38de0956ce58fbf06c6f91a393dc241
Daniel Cao <46983210+dcao182@users.noreply.github.com>
2022-06-24 15:57:49 -0700

    Delete Homework2.py

--- Game of Nim/Homework2.py
+++
@@ -1,8 +0,0 @@
-from search import *
-
-eight_puzzle = EightPuzzle((1, 2, 3, 5, 7, 4, 8, 6, 0))
-
-if __name__ == '__main__':
-    # print(astar_search(eight_puzzle, h=None, display=True).solution())
-    # print(breadth_first_graph_search(eight_puzzle).solution())
-    print(depth_first_graph_search(eight_puzzle).solution())
